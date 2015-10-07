8b527fabda54d24b368aab1fd5204e4774b4f59b
shwetapurushe <shwetapurushe@gmail.com>
2015-10-07 19:44:33 -0400

    cleanup

--- inst/htmlwidgets/d3Scatterplot.js
+++ inst/htmlwidgets/d3Scatterplot.js
@@ -19,19 +19,19 @@ HTMLWidgets.widget({
   renderValue: function(el, p, instance) {
       p.config.container = el.id;
 
-
-    var keys = Object.keys(p.config.data);
+      var keys = Object.keys(p.config.data);
       var cols = p.config.data
       var counter = cols[keys[0]].length;
       var rows = [];
-    for(var i = 0;i< counter; i++){//will run four times
+
+      for(var i = 0;i< counter; i++){//will run four times
         var obj = {};
         for(var j = 0; j<keys.length; j++){
             obj[keys[j]] = cols[keys[j]][i];
         }
         rows.push(obj);
+      }
 
-    }
       console.log(rows);
       p.config.data = rows;
       p.config.columns = {x:keys[0],y:keys[1],key:keys[0]};
@@ -39,11 +39,11 @@ HTMLWidgets.widget({
 
 
 
-	console.log("el", el);
-	console.log("p", p);
-	console.log("instance", instance);
-	//chart.setXAttribute(p.config.columns.x)
-	//chart..setYAttribute(p.config.columns.y)
+      console.log("el", el);
+      console.log("p", p);
+      console.log("instance", instance);
+      //chart.setXAttribute(p.config.columns.x)
+      //chart..setYAttribute(p.config.columns.y)
 
   },
 
