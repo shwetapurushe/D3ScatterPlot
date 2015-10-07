20472b18e5d6517c51e6198ed4760610ae4228ad
shwetapurushe <shwetapurushe@gmail.com>
2015-10-07 19:37:34 -0400

    processing dataframe for d3 splot library

--- inst/htmlwidgets/d3Scatterplot.js
+++ inst/htmlwidgets/d3Scatterplot.js
@@ -1,3 +1,4 @@
+var shweta;
 HTMLWidgets.widget({
 
   name: 'd3Scatterplot',
@@ -5,9 +6,6 @@ HTMLWidgets.widget({
   type: 'output',
 
   initialize: function(el, width, height) {
-	
-	var chart = new d3Chart.Scatterplot();
-	
     return {
      el:el,
 	 width: width,
@@ -19,6 +17,28 @@ HTMLWidgets.widget({
   //p contains the data needed
   //instance is the obj of d3 scatter plot
   renderValue: function(el, p, instance) {
+      p.config.container = el.id;
+
+
+    var keys = Object.keys(p.config.data);
+      var cols = p.config.data
+      var counter = cols[keys[0]].length;
+      var rows = [];
+    for(var i = 0;i< counter; i++){//will run four times
+        var obj = {};
+        for(var j = 0; j<keys.length; j++){
+            obj[keys[j]] = cols[keys[j]][i];
+        }
+        rows.push(obj);
+
+    }
+      console.log(rows);
+      p.config.data = rows;
+      p.config.columns = {x:keys[0],y:keys[1],key:keys[0]};
+      var chart = new d3Chart.Scatterplot(p.config);
+
+
+
 	console.log("el", el);
 	console.log("p", p);
 	console.log("instance", instance);
